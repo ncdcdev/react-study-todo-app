de17203dc2020c50e9f674bad886213120ca0072
rotosiruto <wataponpon5@gmail.com>
2019-11-11 19:06:12 +0900

    #72 環境構築

--- demo/util/urlComponetNameConversion.tsx
+++
@@ -1,9 +0,0 @@
-export default (str, target) => {
-  if (str.slice(0, 1) !== '/') {
-    return false;
-  }
-
-  const text = str.slice(1);
-  const ary = text.split('/')[target];
-  return ary.charAt(0).toUpperCase() + ary.slice(1);
-};

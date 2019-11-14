965b0c6094207cc543c78db486a554e1b0b894f0
rotosiruto <wataponpon5@gmail.com>
2019-11-14 09:17:53 +0900

    #72 環境構築

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import { TextField, Button } from '@material-ui/core';
+import { TextField, Button, TextareaAutosize } from '@material-ui/core';
 
 const Form = props => (
-  <form className="siimple-form">
-    <div className="siimple-form-field">
-      <TextField
-        id="standard-full-width"
-        label="タイトル"
-        style={{ margin: 8 }}
-        placeholder="歯磨き"
-        helperText="Full width!"
-        fullWidth
-        margin="normal"
-        InputLabelProps={{
-          shrink: true,
-        }}
-      />
-      <Button variant="contained" color="primary" onClick={console.log(333)}>
-        送信
-      </Button>
-    </div>
-  </form>
+  <div className="siimple-form-field">
+    <TextField
+      id="standard-full-width"
+      label="タイトル"
+      style={{ margin: 8 }}
+      placeholder="歯磨き"
+      helperText="Full width!"
+      fullWidth
+      margin="normal"
+      InputLabelProps={{
+        shrink: true,
+      }}
+    />
+    <TextareaAutosize aria-label="minimum height" rows={3} placeholder="Minimum 3 rows" />
+    <Button variant="contained" color="primary" onClick={console.log(333)}>
+      送信
+    </Button>
+  </div>
 );
 
 export default Form;
